68d2a4cf8f24132f2202617a3f2fcde2766ef682
Raihan Aria <raihan.aria.ft21@mail.umy.ac.id>
2022-12-15 16:15:56 +0700

    add class queses

--- Circular_Queues/Program.cs
+++ Circular_Queues/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace Circular_Queues
 {
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
