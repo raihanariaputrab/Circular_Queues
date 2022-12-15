49f6c0f0f842a0c82d511468d6c4ac2da204a2b3
Raihan Aria <raihan.aria.ft21@mail.umy.ac.id>
2022-12-15 16:39:38 +0700

    add method to display this program

--- Circular_Queues/Program.cs
+++ Circular_Queues/Program.cs
@@ -93,11 +93,47 @@ namespace Circular_Queues
                 Console.WriteLine();
             }
         }
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            Queues queue = new Queues();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Implement insert operation");
+                    Console.WriteLine("2. Implement delete operation");
+                    Console.WriteLine("3. Display values");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice: ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            Console.WriteLine("Enter a number;  ");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            queue.insert(num);
+                            break;
+                        case '2':
+                            queue.remove();
+                            break;
+                        case '3':
+                            queue.display();
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            Console.WriteLine("Invalid Option");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered.");
+                }
+            }
         }
     }
 }
